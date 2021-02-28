84c62d953258bd118bc1f317a9c582a4372734c4
Lemjo <jochen.lemmens16@gmail.com>
2021-02-28 15:36:03 +0100

    Updated projetct with Code Jo

---
+++ app/src/main/java/com/example/agenda/DBAdapter.java
@@ -0,0 +1,142 @@
+package com.example.agenda;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+
+public class DBAdapter {
+    public static final String KEY_ROWID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_SURNAME = "surname";
+
+    private static final String TAG = "PersonsDbAdapter";
+    private DatabaseHelper mDbHelper;
+    private SQLiteDatabase mDb;
+
+    private static final String DATABASE_NAME = "Agenda";
+    private static final String SQLITE_TABLE = "People";
+    private static final int DATABASE_VERSION = 1;
+
+    private final Context mCtx;
+
+    private static final String DATABASE_CREATE =
+            "CREATE TABLE if not exists " + SQLITE_TABLE + " (" +
+                    KEY_ROWID + " integer PRIMARY KEY autoincrement," +
+                    KEY_NAME + "," +
+                    KEY_SURNAME + "," +
+                    " UNIQUE (" + KEY_ROWID +"));";
+
+    public void deletePerson(String NameToDelete) {
+        mDb.delete(SQLITE_TABLE,  "name=?", new String[]{NameToDelete});
+    }
+
+
+    private static class DatabaseHelper extends SQLiteOpenHelper {
+
+        DatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            Log.w(TAG, DATABASE_CREATE);
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            Log.w(TAG, "Upgrading database from version " + oldVersion + " to "
+                    + newVersion + ", which will destroy all old data");
+            db.execSQL("DROP TABLE IF EXISTS " + SQLITE_TABLE);
+            onCreate(db);
+        }
+    }
+
+
+    public DBAdapter(Context ctx) {
+        this.mCtx = ctx;
+    }
+
+    public DBAdapter open() throws SQLException {
+        mDbHelper = new DatabaseHelper(mCtx);
+        mDb = mDbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        if (mDbHelper != null) {
+            mDbHelper.close();
+        }
+    }
+
+
+    public long createPerson(String name, String surname) {
+
+        ContentValues initialValues = new ContentValues();
+        initialValues.put(KEY_NAME, name);
+        initialValues.put(KEY_SURNAME, surname);
+
+        return mDb.insert(SQLITE_TABLE, null, initialValues);
+    }
+
+    public boolean deleteAllPersons() {
+
+        int doneDelete = 0;
+        doneDelete = mDb.delete(SQLITE_TABLE, null , null);
+        Log.w(TAG, Integer.toString(doneDelete));
+        return doneDelete > 0;
+
+    }
+
+    public Cursor fetchPersonsByName(String inputText) throws SQLException {
+        Log.w(TAG, inputText);
+        Cursor mCursor = null;
+        if (inputText == null  ||  inputText.length () == 0)  {
+            mCursor = mDb.query(SQLITE_TABLE, new String[] {KEY_ROWID,
+                            KEY_NAME, KEY_SURNAME},
+                    null, null, null, null, null);
+
+        }
+        else {
+            mCursor = mDb.query(true, SQLITE_TABLE, new String[] {KEY_ROWID,
+                            KEY_NAME, KEY_SURNAME},
+                    KEY_NAME + " like '%" + inputText + "%'", null,
+                    null, null, null, null);
+        }
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+
+    }
+
+    public Cursor fetchAllPersons() {
+
+        Cursor mCursor = mDb.query(SQLITE_TABLE, new String[] {KEY_ROWID,
+                        KEY_NAME, KEY_SURNAME},
+                null, null, null, null, null);
+
+        if (mCursor != null) {
+            mCursor.moveToFirst();
+        }
+        return mCursor;
+    }
+
+    public void insertSomePersons() {
+
+        createPerson("Nicolas", "SCHWALLER");
+        createPerson("Jochen", "LEMMENS");
+        createPerson("Evan", "FROUIN");
+        createPerson("Lm", "MOUSSY");
+        createPerson("Richard", "CRESSOL");
+
+    }
+
+}
+
--- app/src/main/java/com/example/agenda/HomePageActivity.java
+++ app/src/main/java/com/example/agenda/HomePageActivity.java
@@ -1,36 +1,161 @@
 package com.example.agenda;
 
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FilterQueryProvider;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 public class HomePageActivity extends AppCompatActivity {
 
-    private Button eAddUser;
+    public DBAdapter dbHelper;
+    private SimpleCursorAdapter dataAdapter;
 
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
-        new DbManager(this);
+
+        // déclaration de la barre d'activité
+        Toolbar toolbar = findViewById(R.id.maBarreOutils);
+        super.setActionBar(toolbar);
+
+        dbHelper = new DBAdapter(this);
+        dbHelper.open();
+
+        // "Nettoyer" la donnée
+        dbHelper.deleteAllPersons();
+        // Ajouter des données
+        dbHelper.insertSomePersons();
+
+        // Générer une ListView de SQLite DB
+        displayListView();
+    }
+
+    // Montrer les personnes de la bdd dans une ListView
+    private void displayListView() {
+        Cursor cursor = dbHelper.fetchAllPersons();
+
+        //Colonnes à lier
+        String[] columns = new String[] {
+                DBAdapter.KEY_NAME,
+                DBAdapter.KEY_SURNAME,
+        };
+
+        // XML views définis avec lequel est lié la donnée
+        int[] to = new int[] {
+                R.id.name,
+                R.id.surname,
+        };
+
+        // Créer l'adapter avec un cursor pointant vers la donnée désirée
+        //avec l'information du layout aussi
+        dataAdapter = new SimpleCursorAdapter(
+                this, R.layout.person_info,
+                cursor,
+                columns,
+                to,
+                0);
+
+        ListView listView = (ListView) findViewById(R.id.listView1);
+        // Adapter vers ListView
+        listView.setAdapter(dataAdapter);
+
+
+        // CODE A DVP POUR ACTTIVITE NICO
+
+
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> listView, View view,
+                                    int position, long id) {
+                //get the cursor, positioned to corresponding row in the result set
+                Cursor cursor = (Cursor)
+                        listView.getItemAtPosition(position);
+                // Get the person's name from the row in the database
+                String personName = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                Toast.makeText(getApplicationContext(),
+                        personName, Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                //get the cursor, positioned to corresponding row in the result set
+                Cursor cursor = (Cursor)
+                        listView.getItemAtPosition(position);
+                // Get the person's name from the row in the database
+                String NameToDelete = cursor.getString(cursor.getColumnIndexOrThrow("name"));
+                dbHelper.deletePerson(NameToDelete);
+                Toast.makeText(HomePageActivity.this, "Delete "+parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+                displayListView();
+                dataAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
+
+        EditText myFilter = (EditText) findViewById(R.id.myFilter);
+        myFilter.addTextChangedListener(new TextWatcher() {
+
+            public void afterTextChanged(Editable s) {
+            }
+
+            public void beforeTextChanged(CharSequence s, int start,
+                                          int count, int after) {
+            }
+
+            public void onTextChanged(CharSequence s, int start,
+                                      int before, int count) {
+                dataAdapter.getFilter().filter(s.toString());
+            }
+        });
+
+        dataAdapter.setFilterQueryProvider(new FilterQueryProvider() {
+            public Cursor runQuery(CharSequence constraint) {
+                return dbHelper.fetchPersonsByName(constraint.toString());
+            }
+        });
+
+        ImageView versAddUser = findViewById(R.id.imageView2);
+        versAddUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePageActivity.this, AddUser.class);
+                //intent.putExtra("DB", (Serializable) dbHelper);
+                startActivityForResult(intent,1);
+            }
+        });
 
     }
 
-    public void startdbapp(View v)
-    {
-        new DbManager(this);
-        startActivity(new Intent(HomePageActivity.this, InsertData.class));
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            displayListView();
+            dataAdapter.notifyDataSetChanged();
+        }
     }
-
-    public void startdbappEvent(View v)
-    {
-        startActivity(new Intent(HomePageActivity.this, AjoutEvent.class));
-    }
-
-
 }
+
+
+
